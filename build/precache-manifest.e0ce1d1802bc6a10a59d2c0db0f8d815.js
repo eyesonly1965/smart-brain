a761e157ed2fac94fde6b802681cfd2f7476270c
Leon Dupuis <567198+eyesonly1965@users.noreply.github.com>
2020-07-14 21:02:54 +0200

    Add files via upload

---
+++ build/precache-manifest.e0ce1d1802bc6a10a59d2c0db0f8d815.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4169e8a19b5cc7607a67ee864efd89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b95b62d1bcf86620a08d",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f405fce7ddd5dc219114",
+    "url": "/static/css/main.66154a9f.chunk.css"
+  },
+  {
+    "revision": "b95b62d1bcf86620a08d",
+    "url": "/static/js/2.e09baa21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e09baa21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f405fce7ddd5dc219114",
+    "url": "/static/js/main.8dcda270.chunk.js"
+  },
+  {
+    "revision": "9828e0e40b57a4acb0ba",
+    "url": "/static/js/runtime-main.ba935e5b.js"
+  }
+]);
